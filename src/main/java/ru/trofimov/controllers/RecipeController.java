65f5d1267852a7c231528755a77e5f147e64144b
Mikle <ussass113@gmail.com>
2020-08-04 14:07:41 +0300

    Делаем добавлениен рецепта

--- src/main/java/ru/trofimov/controllers/RecipeController.java
+++ src/main/java/ru/trofimov/controllers/RecipeController.java
@@ -1,8 +1,12 @@
 package ru.trofimov.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/recipe")
@@ -11,5 +15,34 @@ public class RecipeController {
     @GetMapping("/main")
     public String showMain(){
         return "recipe/recipes";
+    }
+
+    @GetMapping("/list")
+    public String showList(){
+        return "recipe/list";
+    }
+
+    @GetMapping("/add")
+    public String showAdd(){
+        return "recipe/add";
+    }
+
+    @PostMapping("/add")
+    public String postAdd(
+            Model model,
+            @RequestParam String recipeName,
+            @RequestParam int category,
+            @RequestParam int listportion,
+            @RequestParam int listhour,
+            @RequestParam int listminut,
+            @RequestParam MultipartFile photo
+            ){
+        System.out.println("I'm here!");
+        System.out.println("RecipeName: " + recipeName);
+        System.out.println("Category: " + category);
+        System.out.println("Listportion: " + listportion);
+        System.out.println("Time: " + listhour + ":" + listminut);
+        System.out.println(photo.getResource().getFilename());
+        return "redirect:/recipe/add";
     }
 }
