826f59e3ea31ba49753b7de1dd7237e9222edb55
Mikle <ussass113@gmail.com>
2020-09-02 13:31:56 +0300

    mobile menu is done

--- src/main/java/ru/trofimov/controllers/MainController.java
+++ src/main/java/ru/trofimov/controllers/MainController.java
@@ -1,16 +1,18 @@
 package ru.trofimov.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import ru.trofimov.config.AppConfig;
 import ru.trofimov.model.DirtyJob;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String showIndex(@RequestHeader("User-Agent") String userAgent){
-        System.out.println(DirtyJob.isMobile(userAgent));
+    public String showIndex(@RequestHeader("User-Agent") String userAgent, Model model){
+        model.addAttribute("color", AppConfig.getColor());
         return DirtyJob.isMobile(userAgent) ? "indexMobile" : "index";
     }
 
