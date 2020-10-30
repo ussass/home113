c56d1043c1d5f0d52f762e21dc6b5b1a22763115
Mikle <ussass113@gmail.com>
2020-10-30 10:07:49 +0300

    add db

---
+++ db/db.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE config_lesson ENCODING 'UTF-8';
+
+CREATE TABLE public.recipes
+(
+    id integer NOT NULL DEFAULT nextval('id_seq'::regclass),
+    recipename character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    category smallint NOT NULL,
+    portion smallint NOT NULL,
+    cookingtime integer NOT NULL,
+    ingredients text COLLATE pg_catalog."default" NOT NULL,
+    steps text COLLATE pg_catalog."default" NOT NULL,
+    photos text COLLATE pg_catalog."default" NOT NULL,
+    datecreate integer NOT NULL,
+    CONSTRAINT recipes_pkey PRIMARY KEY (id)
+)
+
+    TABLESPACE pg_default;
+
+ALTER TABLE public.recipes
+    OWNER to postgres;
